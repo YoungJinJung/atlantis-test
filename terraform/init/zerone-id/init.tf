24b01048d8218f27194109a7ac8416fb2f4d15aa
kuma.jung <yj.jung5078@gmail.com>
2024-08-24 19:16:31 +0900

    Test

--- terraform/init/zerone-id/init.tf
+++ terraform/init/zerone-id/init.tf
@@ -28,3 +28,14 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "terraform_state_locks" {
+  name         = "terraform-locks"
+  hash_key     = "LockID"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
